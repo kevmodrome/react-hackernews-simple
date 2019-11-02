541bb2442e4c0ff4180e96dc6c4e1a0d470a5ab2
Kevin <kevin.aberg@gmail.com>
2019-11-02 10:02:03 +0100

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+
+import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../logo.svg';
+
+const Navigation = () => {
+  return (
+    <nav style={{ background: 'orange', display: 'grid', gridTemplateColumns: 'auto 1fr', padding: '10px 0' }}>
+      <img style={{ height: '30px' }} src={logo} alt="Logo" />
+      <ul style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', listStyle: 'none', margin: 0, alignItems: 'center' }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
